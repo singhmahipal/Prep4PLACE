e64ec9b844d3b1f78e13988bb5d824238020fb1d
singhmahipal <mahipalsingh052025@gmail.com>
2026-04-06 18:49:03 +0530

    added deployment config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,28 @@
 import express from "express";
-import {ENV} from "./lib/env.js";
+import path from "path";
+import { ENV } from "./lib/env.js";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 console.log(ENV.PORT);
 
 app.get("/health", (req, res) => {
-    res.status(200).json({msg: "api is up and running"});
-})
+  res.status(200).json({ msg: "api is up and running" });
+});
+
+app.get("/book", (req, res) => {
+  res.status(200).json({ msg: "book end point" });
+});
+
+// Serve React frontend
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  // catch all unmatched routes
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(ENV.PORT, () => console.log("server listening on port ", ENV.PORT));
