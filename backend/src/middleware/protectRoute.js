55138d1ffa6c2824fe1a9a3b4649dad36aee0f75
singhmahipal <mahipalsingh052025@gmail.com>
2026-04-16 19:30:18 +0530

    fix: auth guard and user mapping issues

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -7,20 +7,22 @@ export const protectRoute = [
         try {
             const clerkId = req.auth().userId;
 
-            if (!clerkId) req.status(401).json({message: "Unauthorized - invalid token"});
+            if (!clerkId) {
+                res.status(401).json({message: "Unauthorized - invalid token"})
+            };
 
             // find user in db using clerkId
-            const user = User.findById({clerkId});
+            const user = await User.findOne({clerkId});
 
             if (!user) return res.status(404).json({message: "User not found"});
 
             // attach user to req
             req.user = user;
 
-            next();
+            return next();
         } catch (error) {
             console.log("Error in protectRoute middleware", error);
-            res.status(500).json({message: "Internal Server Error"});
+            return res.status(500).json({message: "Internal Server Error"});
         }
     }
 ]
