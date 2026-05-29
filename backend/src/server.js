e3de55d0e5d67d124f1147e96bfa80d77926cd40
singhmahipal <mahipalsingh052025@gmail.com>
2026-05-29 13:56:33 +0530

    fix: vercel reload 404 error

--- backend/src/server.js
+++ backend/src/server.js
@@ -31,7 +31,7 @@ app.get("/health", (req, res) => {
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   // catch all unmatched routes
-  app.get("/{*any}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
